f2efa449ff8258e620e28789b230c647ff916358
sakthi170486 <sakthipit@gmail.com>
2019-01-25 19:06:39 +0530

    commit all

---
+++ js/dashboard.js
@@ -0,0 +1,34 @@
+var app = {
+	renderHome: function() {
+		var dashboardTpl = Handlebars.compile($("#dashboard-tpl").html());
+		$('.getcontent').html(dashboardTpl);
+    },	
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+	// deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        document.addEventListener("backbutton", function(e){			
+			if(window.localStorage.getItem('firstpage') == 1){
+				e.preventDefault();
+				navigator.app.exitApp();
+			} else {
+				navigator.app.backHistory()
+			} 
+		}, false);
+    },
+	
+    initialize: function() {
+		var self = this;
+		this.bindEvents();
+		self.renderHome();		
+    }
+};
+app.initialize();
--- js/main.js
+++ js/main.js
@@ -7,12 +7,35 @@ var app = {
 			$('.maincontent').html('');
 			$( ".maincontent" ).effect( 'slide', options, 300, function(){
 				$('.maincontent').html(app.homeTpl());
+				window.localStorage.setItem('firstpage',1);
             });			
 		 }, 3500); 
 		
-    },		
+    },	
+	// Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+	// deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        document.addEventListener("backbutton", function(e){			
+			if(window.localStorage.getItem('firstpage') == 1){
+				e.preventDefault();
+				navigator.app.exitApp();
+			} else {
+				navigator.app.backHistory()
+			} 
+		}, false);
+    },
     initialize: function() {
 		var self = this;
+		this.bindEvents();
 		this.welcomeTpl = Handlebars.compile($("#welcome-tpl").html());
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
 		self.renderHome();
